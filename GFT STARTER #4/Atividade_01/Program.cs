9de31bdaf75499c557ab17972dea767d0fed682e
absjuniordev <abs.junnior@hotmail.com>
2022-04-08 16:40:48 -0300

    Atividade_01

---
+++ GFT STARTER #4/Atividade_01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Atividade_01
+{
+    class Progan
+    {
+        static void Main()
+        {
+            int j = 1;
+            int[] valor = new int[3];
+            Console.WriteLine("Atividade 1, ordenando valores");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Digite o {j}ª valor: ");
+                valor[i] = int.Parse(Console.ReadLine());
+                j++;
+            }
+            Console.WriteLine("Os valores em ordem crescente são:");
+            int[] valorCrescente = valor.OrderBy(i => i).ToArray();
+            foreach (int item in valorCrescente)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Os valores em ordem decrescente são:");
+            int[] valorDecrescente = valor.OrderByDescending(i => i).ToArray();
+            foreach (int item in valorDecrescente)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
